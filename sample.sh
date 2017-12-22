c207f8b248b60dddecc7b169bc4794f8c565bfcf
Kousuke Fujii <fujii@sharecoto.co.jp>
2017-12-22 18:12:43 +0900

    不要なファイル

--- sample.sh
+++
@@ -1,7 +0,0 @@
-curl 'https://slack.com/api/chat.postMessage' \
--F 'token=xoxp-7822924422-7823194065-49407766546-56ff65301a' \
--F 'channel=#sandbox' \
--F 'as_user=true' \
--F 'unfurl_media=true' \
--F 'text=テスト。https://natalie.mu/music/news/261706' \
-
